43b2d33108fbf3a970ca3c3c34592b8fc34eb481
ashen-develops <ashenp.designs@gmail.com>
2020-10-08 16:26:35 -0500

    added notes

--- src/AddNote.js
+++ src/AddNote.js
@@ -10,4 +10,38 @@ class AddNote extends React.Component {
   handleText = (evt) => {
     this.setState({ text: evt.currentTarget.value });
   };
+  handleSubmit = (evt) => {
+      evt.preventDefault();
+      this.context.addNote(this.state.name).then(() => {
+          this.props.history.push("/");
+      });
+  };
+  render(){
+      //create array of option tags parallelling array of folders
+      //map function through the folders
+      //folders.map(folder => {
+        //return <option value={folder name}>{folder name}</option>
+      //})
+        
+      return (
+          <form className="add-note" onSubmit={this.handleSubmit}>
+            <input
+                type="text"
+                placeholder="Name of note"
+                value={this.state.note}
+                onchange={this.handleNote}>
+            </input>
+            <input
+                type="textArea"
+                placeholder="Description"
+                value={this.state.text}
+                onChange={this.handleText}>
+
+            </input>
+            <select>
+                <option value="...">...</option>
+            </select>
+        </form>
+      )
+  }
 }
