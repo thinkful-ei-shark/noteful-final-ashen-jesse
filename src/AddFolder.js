b6769cd4b2d4e2c173c480df199af2deb9db9fa6
Jesse Smart <jessemillersmart@gmail.com>
2020-10-08 16:56:07 -0400

    commit

--- src/AddFolder.js
+++ src/AddFolder.js
@@ -1,1 +1,32 @@
-import React from 'react'
+import { faFolder } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import ApiContext from "./ApiContext.js";
+
+class AddFolder extends React.Component {
+  static contextType = ApiContext;
+  state = { name: "" };
+  handleNameChange = (evt) => {
+    this.setState({ name: evt.currentTarget.value });
+  };
+  handleSubmit = (evt) => {
+    evt.preventDefault();
+    this.context.addFolder(this.state.name).then(() => {
+      this.props.history.push("/");
+    });
+  };
+  render() {
+    return (
+      <form className="add-folder" onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="folder name here"
+          value={this.state.name}
+          onChange={this.handleNameChange}
+        ></input>
+        <button>submit</button>
+      </form>
+    );
+  }
+}
+
+export default AddFolder;
--- src/AddNote.js
+++ src/AddNote.js
@@ -1,1 +1,13 @@
-import React from 'react'
+import React from "react";
+import ApiContext from "./ApiContext.js";
+
+class AddNote extends React.Component {
+  static contextType = ApiContext;
+  state = { note: "", text: "" };
+  handleNote = (evt) => {
+    this.setState({ note: evt.currentTarget.value });
+  };
+  handleText = (evt) => {
+    this.setState({ text: evt.currentTarget.value });
+  };
+}
