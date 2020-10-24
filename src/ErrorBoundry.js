8686d8a2f264f1b2ffa2b642d58815bc6d0920a7
ashen-develops <ashenp.designs@gmail.com>
2020-10-24 12:30:49 -0500

    added error boundry

--- src/ErrorBoundry.js
+++ src/ErrorBoundry.js
@@ -1,1 +1,27 @@
 import React from 'react'
+import PropTypes from 'prop-types';
+class ErrorBoundary extends React.Component {
+  static defaultProps = {
+    item: ''
+  }
+
+  state = {
+    hasError: false
+  }
+
+  static getDerivedStateFromError(error) {
+    return {hasError: true};
+  }
+  render() {
+    if (this.state.hasError) {
+      return <h2>{`Could not display this ${this.props.item} section`}</h2>;
+    }
+    return this.props.children;
+  }
+}
+
+ErrorBoundary.propTypes = {
+  item: PropTypes.string
+}
+
+export default ErrorBoundary;
